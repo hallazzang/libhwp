100fccbb82e2628d07a79710552d153760e38b86
Hodong Kim <cogniti@gmail.com>
2014-04-20 05:50:42 +0900

    * apply GPL, poppler * add hwp_page_get_selected_text, hwp_page_get_selected_region, hwp_page_render_selection, hwp_page_render_for_printing, hwp_page_find_text, hwp_rectangle, hwp_color

--- src/hwp-page.c
+++ src/hwp-page.c
@@ -4,18 +4,18 @@
  *
  * Copyright (C) 2012-2014 Hodong Kim <hodong@cogno.org>
  *
- * This library is free software: you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published
- * by the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
  *
- * This library is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU Lesser General Public License for more details.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "hwp-page.h"
@@ -37,10 +37,67 @@ void hwp_page_get_size (HwpPage *page, gdouble *width, gdouble *height)
 {
   g_return_if_fail (HWP_IS_PAGE (page));
 
-  *width  = 595.0;
-  *height = 842.0;
+  poppler_page_get_size (page->poppler_page, width, height);
 }
 
+/**
+ * hwp_page_get_selected_text:
+ * @page: a #HwpPage
+ * @style: a #HwpSelectionStyle
+ * @selection: the #HwpRectangle including the text
+ *
+ * Retrieves the contents of the specified @selection as text.
+ *
+ * Return value: a pointer to the contents of the @selection
+ *               as a string
+ * Since: 0.0.3
+ **/
+char *hwp_page_get_selected_text (HwpPage          *page,
+                                  HwpSelectionStyle style,
+                                  HwpRectangle     *selection)
+{
+  g_return_val_if_fail (HWP_IS_PAGE (page), NULL);
+  g_return_val_if_fail (selection != NULL, NULL);
+
+  return poppler_page_get_selected_text (POPPLER_PAGE (page->poppler_page),
+                                         (PopplerSelectionStyle) style,
+                                         (PopplerRectangle *) selection);
+}
+
+/**
+ * Since: 0.0.3
+ */
+cairo_region_t *
+hwp_page_get_selected_region (HwpPage          *page,
+                              gdouble           scale,
+                              HwpSelectionStyle style,
+                              HwpRectangle     *selection)
+{
+  return poppler_page_get_selected_region (page->poppler_page,
+                                           scale,
+                                           (PopplerSelectionStyle)  style,
+                                           (PopplerRectangle *) selection);
+}
+
+/**
+ * Since: 0.0.3
+ */
+void hwp_page_render_selection (HwpPage          *page,
+                                cairo_t          *cairo,
+                                HwpRectangle     *selection,
+                                HwpRectangle     *old_selection,
+                                HwpSelectionStyle style,
+                                HwpColor         *glyph_color,
+                                HwpColor         *background_color)
+{
+  poppler_page_render_selection (page->poppler_page,
+                                 cairo,
+                                 (PopplerRectangle *) selection,
+                                 (PopplerRectangle *) old_selection,
+                                 (PopplerSelectionStyle) style,
+                                 (PopplerColor *) glyph_color,
+                                 (PopplerColor *) background_color);
+}
 /**
  * hwp_page_new:
  *
@@ -50,13 +107,16 @@ void hwp_page_get_size (HwpPage *page, gdouble *width, gdouble *height)
  */
 HwpPage *hwp_page_new (void)
 {
-  return (HwpPage *) g_object_new (HWP_TYPE_PAGE, NULL);
+  return g_object_new (HWP_TYPE_PAGE, NULL);
 }
 
 static void hwp_page_finalize (GObject *object)
 {
   HwpPage *page = HWP_PAGE (object);
-  g_array_free (page->paragraphs, TRUE);
+
+  if (page->poppler_page)
+    g_object_unref (page->poppler_page);
+
   G_OBJECT_CLASS (hwp_page_parent_class)->finalize (object);
 }
 
@@ -68,5 +128,138 @@ static void hwp_page_class_init (HwpPageClass *klass)
 
 static void hwp_page_init (HwpPage *page)
 {
-  page->paragraphs = g_array_new (TRUE, TRUE, sizeof (HwpParagraph *));
+}
+
+/**
+ * hwp_page_render:
+ * @page: the page to render from
+ * @cr: cairo context to render to
+ *
+ * Render the page to the given cairo context. This function
+ * is for rendering a page that will be displayed. If you want
+ * to render a page that will be printed use
+ * hwp_page_render_for_printing() instead
+ *
+ * Since: 0.0.3
+ */
+void hwp_page_render (HwpPage *page, cairo_t *cr)
+{
+  g_return_if_fail (HWP_IS_PAGE (page));
+  g_return_if_fail (cr);
+
+  poppler_page_render (page->poppler_page, cr);
+}
+
+/**
+ * Since: 0.0.3
+ */
+void hwp_page_render_for_printing (HwpPage *page,
+                                   cairo_t *cairo)
+{
+  g_return_if_fail (HWP_IS_PAGE (page));
+
+  poppler_page_render_for_printing (page->poppler_page, cairo);
+}
+
+/**
+ * hwp_page_find_text:
+ * @page: a #HwpPage
+ * @text: the text to search for (UTF-8 encoded)
+ *
+ * A #GList of rectangles for each occurance of the text on the page.
+ * The coordinates are in HWP points.
+ *
+ * Return value: (element-type HwpRectangle) (transfer full): a #GList of #HwpRectangle,
+ **/
+GList *hwp_page_find_text (HwpPage *page, const char *text)
+{
+  return poppler_page_find_text (page->poppler_page, text);
+}
+
+/* HwpRectangle type */
+
+HWP_DEFINE_BOXED_TYPE (HwpRectangle, hwp_rectangle,
+                       hwp_rectangle_copy,
+                       hwp_rectangle_free)
+
+/**
+ * hwp_rectangle_new:
+ *
+ * Creates a new #HwpRectangle
+ *
+ * Returns: a new #HwpRectangle, use hwp_rectangle_free() to free it
+ */
+HwpRectangle *hwp_rectangle_new (void)
+{
+  return g_slice_new0 (HwpRectangle);
+}
+
+/**
+ * hwp_rectangle_copy:
+ * @rectangle: a #HwpRectangle to copy
+ *
+ * Creates a copy of @rectangle
+ *
+ * Returns: a new allocated copy of @rectangle
+ */
+HwpRectangle *hwp_rectangle_copy (HwpRectangle *rectangle)
+{
+  g_return_val_if_fail (rectangle != NULL, NULL);
+
+  return g_slice_dup (HwpRectangle, rectangle);
+}
+
+/**
+ * hwp_rectangle_free:
+ * @rectangle: a #HwpRectangle
+ *
+ * Frees the given #HwpRectangle
+ */
+void hwp_rectangle_free (HwpRectangle *rectangle)
+{
+  g_slice_free (HwpRectangle, rectangle);
+}
+
+/* HwpColor type */
+HWP_DEFINE_BOXED_TYPE (HwpColor, hwp_color, hwp_color_copy, hwp_color_free)
+
+/**
+ * hwp_color_new:
+ *
+ * Creates a new #HwpColor
+ *
+ * Returns: a new #HwpColor, use hwp_color_free() to free it
+ */
+HwpColor *hwp_color_new (void)
+{
+  return (HwpColor *) g_new0 (HwpColor, 1);
+}
+
+/**
+ * hwp_color_copy:
+ * @color: a #HwpColor to copy
+ *
+ * Creates a copy of @color
+ *
+ * Returns: a new allocated copy of @color
+ */
+HwpColor *hwp_color_copy (HwpColor *color)
+{
+  HwpColor *new_color;
+
+  new_color = g_new (HwpColor, 1);
+  *new_color = *color;
+
+  return new_color;
+}
+
+/**
+ * hwp_color_free:
+ * @color: a #HwpColor
+ *
+ * Frees the given #HwpColor
+ */
+void hwp_color_free (HwpColor *color)
+{
+  g_free (color);
 }
--- src/hwp-page.h
+++ src/hwp-page.h
@@ -4,18 +4,18 @@
  *
  * Copyright (C) 2012-2014 Hodong Kim <hodong@cogno.org>
  *
- * This library is free software: you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published
- * by the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
  *
- * This library is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU Lesser General Public License for more details.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; If not, see <http://www.gnu.org/licenses/>.
  */
 
 #if !defined (__HWP_H_INSIDE__) && !defined (HWP_COMPILATION)
@@ -29,6 +29,7 @@
 #include <cairo.h>
 
 #include "hwp-enums.h"
+#include <poppler.h>
 
 G_BEGIN_DECLS
 
@@ -42,10 +43,14 @@ G_BEGIN_DECLS
 typedef struct _HwpPage       HwpPage;
 typedef struct _HwpPageClass  HwpPageClass;
 
+typedef struct _HwpRectangle  HwpRectangle;
+typedef struct _HwpColor      HwpColor;
+
 struct _HwpPage
 {
   GObject parent_instance;
-  GArray *paragraphs;
+
+  PopplerPage *poppler_page;
 };
 
 struct _HwpPageClass
@@ -53,12 +58,103 @@ struct _HwpPageClass
   GObjectClass parent_class;
 };
 
-GType    hwp_page_get_type    (void) G_GNUC_CONST;
-HwpPage *hwp_page_new         (void);
-void     hwp_page_get_size    (HwpPage *page,
-                               gdouble *width,
-                               gdouble *height);
-gboolean  hwp_page_render     (HwpPage *page,    cairo_t *cr);
+GType    hwp_page_get_type   (void) G_GNUC_CONST;
+HwpPage *hwp_page_new        (void);
+void     hwp_page_get_size   (HwpPage          *page,
+                              gdouble          *width,
+                              gdouble          *height);
+void     hwp_page_render     (HwpPage          *page,
+                              cairo_t          *cr);
+void
+hwp_page_render_selection    (HwpPage          *page,
+                              cairo_t          *cairo,
+                              HwpRectangle     *selection,
+                              HwpRectangle     *old_selection,
+                              HwpSelectionStyle style,
+                              HwpColor         *glyph_color,
+                              HwpColor         *background_color);
+char *
+hwp_page_get_selected_text   (HwpPage          *page,
+                              HwpSelectionStyle style,
+                              HwpRectangle     *selection);
+cairo_region_t *
+hwp_page_get_selected_region (HwpPage          *page,
+                              gdouble           scale,
+                              HwpSelectionStyle style,
+                              HwpRectangle     *selection);
+GList *hwp_page_find_text    (HwpPage          *page,
+                              const char       *text);
+void
+hwp_page_render_for_printing (HwpPage          *page,
+                              cairo_t          *cairo);
+
+/*
+ * A convenience macro for boxed type implementations, which defines a
+ * type_name_get_type() function registering the boxed type.
+ */
+#define HWP_DEFINE_BOXED_TYPE(TypeName, type_name, copy_func, free_func)          \
+GType                                                                                 \
+type_name##_get_type (void)                                                           \
+{                                                                                     \
+        static volatile gsize g_define_type_id__volatile = 0;                         \
+	if (g_once_init_enter (&g_define_type_id__volatile)) {                        \
+	        GType g_define_type_id =                                              \
+		    g_boxed_type_register_static (g_intern_static_string (#TypeName), \
+		                                  (GBoxedCopyFunc) copy_func,         \
+						  (GBoxedFreeFunc) free_func);        \
+		g_once_init_leave (&g_define_type_id__volatile, g_define_type_id);    \
+	}                                                                             \
+	return g_define_type_id__volatile;                                            \
+}
+
+/* A rectangle on a page */
+#define HWP_TYPE_RECTANGLE             (hwp_rectangle_get_type ())
+/**
+ * HwpRectangle:
+ * @x1: x coordinate of lower left corner
+ * @y1: y coordinate of lower left corner
+ * @x2: x coordinate of upper right corner
+ * @y2: y coordinate of upper right corner
+ *
+ * A #HwpRectangle is used to describe
+ * locations on a page and bounding boxes
+ */
+struct _HwpRectangle
+{
+  gdouble x1;
+  gdouble y1;
+  gdouble x2;
+  gdouble y2;
+};
+
+GType         hwp_rectangle_get_type (void) G_GNUC_CONST;
+HwpRectangle *hwp_rectangle_new      (void);
+HwpRectangle *hwp_rectangle_copy     (HwpRectangle *rectangle);
+void          hwp_rectangle_free     (HwpRectangle *rectangle);
+
+/* A color in RGB */
+#define HWP_TYPE_COLOR                 (hwp_color_get_type ())
+
+/**
+ * HwpColor:
+ * @red: the red componment of color
+ * @green: the green component of color
+ * @blue: the blue component of color
+ *
+ * A #HwpColor describes a RGB color. Color components
+ * are values between 0 and 65535
+ */
+struct _HwpColor
+{
+  guint16 red;
+  guint16 green;
+  guint16 blue;
+};
+
+GType     hwp_color_get_type (void) G_GNUC_CONST;
+HwpColor *hwp_color_new      (void);
+HwpColor *hwp_color_copy     (HwpColor *color);
+void      hwp_color_free     (HwpColor *color);
 
 G_END_DECLS
 
