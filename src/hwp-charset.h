b2f7f8ad1efdcbc6474d3a4e16450b56affa69be
Hodong Kim <cogniti@gmail.com>
2015-10-02 23:26:07 +0900

    hwp-charset.h has been dedicated to the public domain in 2015.

--- src/hwp-charset.h
+++ src/hwp-charset.h
@@ -1,21 +1,8 @@
 /* -*- Mode: C; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*- */
 /*
- * hwp-charset.h
+ * hwp-charset.h has been dedicated to the public domain in 2015.
  *
- * Copyright (C) 2013-2014 Hodong Kim <hodong@cogno.org>
- * 
- * This library is free software: you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published
- * by the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This library is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Author: Hodong Kim <hodong@cogno.org>
  */
 
 /*
