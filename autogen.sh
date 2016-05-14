f03f11f36ee2f5a6e9b2f7d068398cfbd7db03a7
Hodong Kim <cogniti@gmail.com>
2016-05-14 15:49:29 +0900

    Check pkg-config, libglib2.0-dev

--- autogen.sh
+++ autogen.sh
@@ -16,6 +16,18 @@ else
   gtkdocize || exit $?
 fi
 
+PKGCONFIG=`which pkg-config`
+if test -z "$PKGCONFIG"; then
+  echo "pkg-config not found, please install pkg-config"
+  exit 1
+fi
+
+pkg-config --print-errors glib-2.0
+if [ $? != 0 ]; then
+  echo "You probably need to install 'libglib2.0-dev'"
+  exit 1
+fi
+
 INTLTOOLIZE=`which intltoolize`
 if test -z $INTLTOOLIZE=; then
   echo "intltoolize not found, please install intltool package"
