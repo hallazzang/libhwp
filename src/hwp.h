92549e4692f3ba1382a3863b899556a9916923e7
Hodong Kim <cogniti@gmail.com>
2016-08-02 17:52:43 +0900

    Changed license to LGPL-3+ or Apache-2.0

--- src/hwp.h
+++ src/hwp.h
@@ -3,7 +3,9 @@
  * hwp.h
  * This file is part of the libhwp project.
  *
- * Copyright (C) 2012-2016 Hodong Kim <hodong@cogno.org>
+ * Copyright (C) 2012-2016 Hodong Kim <cogniti@gmail.com>
+ *
+ * The libhwp is dual licensed under the LGPL v3+ or Apache License 2.0
  *
  * The libhwp is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published
@@ -16,7 +18,21 @@
  * See the GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program;  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Or,
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #ifndef __HWP_H__
